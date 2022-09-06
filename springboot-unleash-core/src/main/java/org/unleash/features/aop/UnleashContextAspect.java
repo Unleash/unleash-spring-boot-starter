7d1222ba7657ca68d0b806b11c9ed928d9a427aa
praveen <praveen.govindan@gmail.com>
2022-09-05 22:00:45 -0700

    Removed unnecessary logs

--- springboot-unleash-core/src/main/java/org/unleash/features/aop/UnleashContextAspect.java
+++ springboot-unleash-core/src/main/java/org/unleash/features/aop/UnleashContextAspect.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.unleash.features.annotation.Context;
 
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
 
 @Aspect
 public class UnleashContextAspect {
